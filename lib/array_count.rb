9e6d49f02df0aca9b0da31679344688c2749f574
Dennis Portillo <dennis.portillo@yale.edu>
2020-05-21 17:34:38 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,13 +1,12 @@
 def count_strings(array)
- if array_1.count do |element|
+ if array.count do |element|
   element.class == String
   end
 end 
 
 
 def count_empty_strings(array)
-  array_1.count do |element|
+  array.count do |element|
      if element.class == String
     element.empty?
   end
-end
